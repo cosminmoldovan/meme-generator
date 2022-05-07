8ef4f39b795dc9ca5c1a27d3a0e38bcc3f29c704
Cosmin Moldovan <cosmin.moldovan@protonmail.com>
2022-05-07 16:01:31 +0300

    clear app extra files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Meme Generator</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+function Navbar(){
+    return (
+        <nav>
+            navbar
+        </nav>
+    );
+}
+export default Navbar;
