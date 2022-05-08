f8d3fbbba2903e8dfed33f806188b3dad7af1429
Cosmin Moldovan <cosmin.moldovan@protonmail.com>
2022-05-08 19:26:02 +0300

    get random image challenge

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,22 @@
+/**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     * 
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+*/
+import memesData  from "../memesData";
 function Meme(){
+    function getImage(){
+        let randomNum = Math.floor(Math.random() * 101);
+        let imgUrl = memesData.data.memes[randomNum].url;
+        console.log(imgUrl);
+    }
     return (
         <div className='form'>
             <input type='text' placeholder="Top text" className='form-input' />
             <input type='text' placeholder="Bottom text"  className='form-input'/>
-           <button className='form-btn'>Get a new meme image 🖼</button>
+           <button className='form-btn' onClick={getImage}>Get a new meme image 🖼</button>
         </div>
     );
 }
