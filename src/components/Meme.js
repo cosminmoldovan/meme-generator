b4de7621a314293ef73ef599ef717ecd46afdfbb
Cosmin Moldovan <cosmin.moldovan@protonmail.com>
2022-05-09 19:34:45 +0300

    add useState to get random image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,24 +1,25 @@
-/**
-     * Challenge: Get a random image from the `memesData` array
-     * when the "new meme image" button is clicked.
-     * 
-     * Log the URL of the image to the console. (Don't worry
-     * about displaying the image yet)
-*/
+import React from 'react'
 import memesData  from "../memesData";
 function Meme(){
+
+    const memesArray = memesData.data.memes
+
+    const [memeImage, setMemeImage] = React.useState(memesArray[2].url);
+
     function getImage(){
-        const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         let imgUrl = memesArray[randomNumber].url;
-        console.log(imgUrl);
+        setMemeImage(imgUrl);
     }
     return (
+        <>
         <div className='form'>
             <input type='text' placeholder="Top text" className='form-input' />
             <input type='text' placeholder="Bottom text"  className='form-input'/>
            <button className='form-btn' onClick={getImage}>Get a new meme image 🖼</button>
         </div>
+        <img src={memeImage} alt="meme" className='meme-image'/>
+        </>
     );
 }
 export default Meme;
