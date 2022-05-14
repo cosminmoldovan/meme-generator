4cfe4751e5cc8e94c23f23b287233fb2416facad
Cosmin Moldovan <cosmin.moldovan@protonmail.com>
2022-05-14 14:15:00 +0300

    add text to image functionality

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -8,7 +8,6 @@ function Meme(){
         randomImage: "https://i.imgflip.com/3si4.jpg"
     })
     const [allMemeImages, setAllMemeImages] = React.useState(memesData);
-
     function getImage(){
         const memesArray = allMemeImages.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
@@ -20,17 +19,38 @@ function Meme(){
             }
         })
     }
+    function handleChange(event){
+        const {name, value} = event.target;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
     return (
         <>
         <div className='form'>
-            <input type='text' placeholder="Top text" className='form-input' />
-            <input type='text' placeholder="Bottom text"  className='form-input'/>
+            <input 
+            type='text' 
+            placeholder="Top text" 
+            className='form-input'
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+             />
+            <input 
+            type='text' 
+            placeholder="Bottom text"  
+            className='form-input'
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+            />
            <button className='form-btn' onClick={getImage}>Get a new meme image 🖼</button>
         </div>
         <div className="meme">
                 <img src={meme.randomImage} className="meme-image" />
-                <h2 className="meme-text top">One does not simply</h2>
-                <h2 className="meme-text bottom">Walk into Mordor</h2>
+                <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
         </div>
         </>
     );
